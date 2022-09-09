5573c8d2f4d226b6f95d71952ac07eea0e5e71c1
Dhruv Saxena <dhruv162002@gmail.com>
2022-09-09 10:42:33 +0530

    Initial Commit

---
+++ app/src/main/java/com/ar/application/InAppMessaging.kt
@@ -0,0 +1,11 @@
+package com.ar.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InAppMessaging : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_in_app_messaging)
+    }
+}
