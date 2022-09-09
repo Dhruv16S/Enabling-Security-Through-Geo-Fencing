afd07619936db18420b250ede593a1138916d42b
Dhruv Saxena <dhruv162002@gmail.com>
2022-09-10 00:00:41 +0530

    Complete SMS Functionality

--- app/src/main/java/com/ar/application/InAppMessaging.kt
+++ app/src/main/java/com/ar/application/InAppMessaging.kt
@@ -1,12 +1,64 @@
 package com.ar.application
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
+import android.telephony.SmsManager
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 class InAppMessaging : AppCompatActivity() {
+    private lateinit var sosButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_in_app_messaging)
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.RECEIVE_SMS, android.Manifest.permission.SEND_SMS), 111)
+        } else{
+            receiveMsg()
+        }
+        sosButton = findViewById(R.id.sos_button)
+        sosButton.setOnClickListener {
+            try {
+                val msgObj = SmsManager.getDefault()
+                // Fetch this number form the database, put your number while testing
+                msgObj.sendTextMessage("8978383634", null, "Sent Via Android Studio", null, null)
+            }catch(e : Exception){
+                Log.d("Error is : ", e.message.toString())
+                Toast.makeText(this, "Doesn't work ${e.message.toString()}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            receiveMsg()
+        }
+    }
+    private fun receiveMsg(){
+        var br = object : BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+                    for(sms in Telephony.Sms.Intents.getMessagesFromIntent(p1)){
+                        Toast.makeText(this@InAppMessaging, sms.displayMessageBody, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+
     }
 }
